d26b3f007d5a4899e6f62e8ef9a74f73d1deb06b
Sim Greenbaum <gbs4ever2@gmail.com>
2019-02-17 05:47:28 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -40,7 +40,18 @@ class Student
     file.name=row[1]
     file.grade=row[2]
   end
-
+  def self.find_by_name(name)
+      sql = <<-SQL
+          SELECT *
+          FROM students
+          WHERE name = ?
+          LIMIT 1
+        SQL
+  
+        DB[:conn].execute(sql, name).map do |row|
+          self.new_from_db(row)
+        end.first
+      end
 
 
 
