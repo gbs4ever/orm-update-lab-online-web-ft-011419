63005509bceb95d9e0c9817ab385fb4b4c3d1e78
Sim Greenbaum <gbs4ever2@gmail.com>
2019-02-17 05:43:27 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -30,7 +30,7 @@ class Student
     @id =DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
   end
   def self.create(name,grade)
-    file=students.new(name,grade)
+    file=Student.new(name,grade)
     file.save
     file
   end
