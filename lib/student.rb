0bb2a21ad1c2456c4b36085534022413620557e1
Sim Greenbaum <gbs4ever2@gmail.com>
2019-02-17 05:50:24 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -2,7 +2,7 @@ require_relative "../config/environment.rb"
 
 class Student
    attr_accessor :id, :name, :grade
-    def initialize(name,grade,id=nil)
+    def initialize
       @name=name
       @grade =grade
       @id = id
